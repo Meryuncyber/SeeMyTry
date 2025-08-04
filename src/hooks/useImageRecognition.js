5c84c53fb031a88ea01e4bb6b913490537a3ea78
MerYunCyber <taymuryunus77@gmail.com>
2025-08-04 17:16:38 +0300

    Update useImageRecognition.js

--- src/hooks/useImageRecognition.js
+++ src/hooks/useImageRecognition.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from 'react';
-import { recognizeImage } from '../services/imageRecognitionService';
+import { recognizeProduct } from '../services/imageRecognitionService';
 
 export const useImageRecognition = () => {
   const [isProcessing, setIsProcessing] = useState(false);
