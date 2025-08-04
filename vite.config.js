d3597a0377f53e83061b7b7cac6883910c666e8f
MerYunCyber <taymuryunus77@gmail.com>
2025-08-04 17:27:47 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path'; // `path` modülünü import ediyoruz
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mtbot/', 
+  base: '/mtbot/',
   server: {
     host: '0.0.0.0',
     port: 3000,
@@ -12,4 +13,11 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
   },
+  // Bu bölümü ekliyoruz:
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  publicDir: 'src/models', // Model dosyalarını public dizini olarak belirtiyoruz.
 });
