89e47019bae1d60f62e77733a30f1dc241a2dd51
MerYunCyber <taymuryunus77@gmail.com>
2025-08-04 14:45:41 +0300

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    // Tailwind CSS'in kendisini bir PostCSS eklentisi olarak ekliyoruz.
+    tailwindcss: {},
+    // Autoprefixer eklentisi, CSS kurallarına otomatik olarak tarayıcı ön eklerini ekler.
+    // Bu sayede projemiz daha geniş bir tarayıcı yelpazesinde sorunsuz çalışır.
+    autoprefixer: {},
+  },
+};
